c11575cd1bfc4219057c4d8bcb80297791637e3f
Vikas Arora <vikasx1912@gmail.com>
2018-03-29 01:14:05 +0530

    Circullar Linked List: Initial Commit

---
+++ circular_linked_list.js
@@ -0,0 +1,48 @@
+function NewNode(data) {
+	this.data = data;
+	this.next = null;	
+}
+
+function CircularLinkedList() {
+	this.head = null;
+}
+
+CircularLinkedList.prototype.addNode = function(data) {
+
+	var node = new NewNode(data),
+		currentNode = this.head;
+		
+
+	if(!currentNode) {
+		this.head = node;
+		node.next = node;
+
+		return node;
+	}
+
+	while(currentNode.next != this.head) {
+		currentNode = currentNode.next;
+	}
+
+	currentNode.next = node;
+	node.next = this.head;
+
+	return node;
+
+}
+
+
+var cll = new CircularLinkedList();
+
+cll.addNode(1);
+cll.addNode(2);
+cll.addNode(3);
+cll.addNode(4);
+cll.addNode(5);
+cll.addNode(6);
+cll.addNode(7);
+cll.addNode(8);
+cll.addNode(9);
+cll.addNode(10);
+
+console.log(cll);
